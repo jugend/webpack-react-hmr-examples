bf82298d4addac20aa4522af88a55d64c89cd04d
Herryanto Siatono <herryanto@gmail.com>
2015-12-30 01:36:57 +0800

    add react-hmre preset to express config

--- webpack.config.express.js
+++ webpack.config.express.js
@@ -26,7 +26,7 @@ module.exports = {
         loader: 'babel',
         include: path.join(__dirname, 'src'),
         query: {
-          presets: ['es2015', 'react', 'stage-0'],
+          presets: ['es2015', 'react', 'stage-0', 'react-hmre'],
         }
       }
     ]
